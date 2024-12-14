0a96fe107a7798f70975a6045fc62d49e2b91b02
Jacson Mendes <jacson.g.mendes@gmail.com>
2024-12-13 23:29:43 -0300

    feat: enable project run in the multi environment

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    defaultCommandTimeout: 10000,
+    requestTimeout: 10000,
+    video: false,
+    screenshotOnRunFailure: true,
+    watchForFileChanges: false,
+    experimentalStudio: false,
+    retries: {
+      runMode: 2,
+      openMode: 0
+    },
+    setupNodeEvents(on, config) {
+    },
+  },
+})
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+import LoginElements from "../support/elements/loginElements";
+const loginElements = new LoginElements();
+import HomeElements from "../support/elements/homeElements";
+const homeElements = new HomeElements();
+
+describe('Login Tests', () => {
+
+  beforeEach(() => {
+    cy.visitLoginPage()
+  })
+
+  it('Should login successfully with valid credentials', () => {
+    cy.loginUser()
+    cy.get(homeElements.btnLogout()).should('be.visible')
+  });
+
+  it('Should show error for invalid credentials', () => {
+    cy.loginUser("invalid@user.com", "wrongpassword")
+    cy.get(loginElements.alertError()).contains('Email e/ou senha inválidos');
+  });
+});
